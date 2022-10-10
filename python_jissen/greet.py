ab7964d35174341e36665cf3db1c4a2e2b312396
tomhara <tom-0714@hotmail.co.jp>
2022-10-10 21:58:18 +0900

    .gitignoreを追加

---
+++ python_jissen/greet.py
@@ -0,0 +1,11 @@
+from email.policy import default
+import click
+
+@click.command()
+@click.option('--words', default='Hello')
+@click.argument('name')
+def greet(name, words):
+    click.echo(f'{words}, {name}!')
+
+if __name__== '__main__':
+    greet()
