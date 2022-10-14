010c3e5e1c574e2668754443a517781046e5247c
tomhara <tom-0714@hotmail.co.jp>
2022-10-14 23:43:04 +0900

    コマンドライン引数を受け取る

--- python_jissen/core.py
+++ python_jissen/core.py
@@ -1,12 +1,14 @@
 import click
 
 @click.command()
-def cli():
+@click.option('--message', '-m', default='LGTM', show_default=True, help='画像に乗せる文字列')
+@click.argument('keyword')
+def cli(keyword, message):
     """LGTM画像生成ツール"""
-    lgtm()
+    lgtm(keyword, message)
     click.echo('lgtm')
 
 
-def lgtm():
+def lgtm(keyword, message):
     pass
 
